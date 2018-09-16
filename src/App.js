95812cf2729415308b1ac8c97ad36649997f7ca7
Nina Olofsson <nina.olofsson@tink.se>
2018-09-16 19:03:09 +0200

    feat: att styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react'
 import { Router, Link } from 'react-static'
+import styled, { injectGlobal } from 'styled-components'
 import { hot } from 'react-hot-loader'
 //
 import Routes from 'react-static-routes'
 
-import './app.css'
+injectGlobal`
+  body {
+    font-family: 'HelveticaNeue-Light', 'Helvetica Neue Light', 'Helvetica Neue', Helvetica, Arial,
+      'Lucida Grande', sans-serif;
+    font-weight: 300;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+  }
+`
+
+const AppStyles = styled.div`
+  a {
+    text-decoration: none;
+    color: #704f56;
+    font-weight: bold;
+  }
+  nav {
+    width: 100%;
+    background: #39ae7f;
+    a {
+      color: white;
+      padding: 1rem;
+      display: inline-block;
+    }
+  }
+  .content {
+    padding: 1rem;
+  }
+  img {
+    max-width: 100%;
+  }
+`
 
 const App = () => (
-  <Router>
-    <div>
-      <nav>
-        <Link exact to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav>
-      <div className="content">
-        <Routes />
-      </div>
-    </div>
-  </Router>
+    <Router>
+      <AppStyles>
+        <nav>
+          <Link exact to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/blog">Blog</Link>
+        </nav>
+        <div className="content">
+          <Routes />
+        </div>
+      </AppStyles>
+    </Router>
 )
 
 export default hot(module)(App)
--- static.config.js
+++ static.config.js
@@ -1,48 +1,73 @@
-import { reloadRoutes } from 'react-static/node'
+import {reloadRoutes} from 'react-static/node'
 import jdown from 'jdown'
 import chokidar from 'chokidar'
+import {ServerStyleSheet} from 'styled-components'
 
 chokidar.watch('content').on('all', () => reloadRoutes())
 
 export default {
-  getSiteData: () => ({
-    title: 'React Static',
-  }),
-  getRoutes: async () => {
-    const { posts, home, about } = await jdown('content')
-    return [
-      {
-        path: '/',
-        component: 'src/containers/Home',
-        getData: () => ({
-          ...home,
-        }),
-      },
-      {
-        path: '/about',
-        component: 'src/containers/About',
-        getData: () => ({
-          about,
-        }),
-      },
-      {
-        path: '/blog',
-        component: 'src/containers/Blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.slug}`,
-          component: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
-      {
-        is404: true,
-        component: 'src/containers/404',
-      },
-    ]
-  },
+    getSiteData: () => ({
+        title: 'React Static',
+    }),
+    getRoutes: async () => {
+        const {posts, home, about} = await jdown('content')
+        return [
+            {
+                path: '/',
+                component: 'src/containers/Home',
+                getData: () => ({
+                    ...home,
+                }),
+            },
+            {
+                path: '/about',
+                component: 'src/containers/About',
+                getData: () => ({
+                    about,
+                }),
+            },
+            {
+                path: '/blog',
+                component: 'src/containers/Blog',
+                getData: () => ({
+                    posts,
+                }),
+                children: posts.map(post => ({
+                    path: `/post/${post.slug}`,
+                    component: 'src/containers/Post',
+                    getData: () => ({
+                        post,
+                    }),
+                })),
+            },
+            {
+                is404: true,
+                component: 'src/containers/404',
+            },
+        ]
+    },
+    renderToHtml: (render, Comp, meta) => {
+        const sheet = new ServerStyleSheet()
+        const html = render(sheet.collectStyles(<Comp/>))
+        meta.styleTags = sheet.getStyleElement()
+        return html
+    },
+    Document: class CustomHtml extends Component {
+        render() {
+            const {
+                Html, Head, Body, children, renderMeta,
+            } = this.props
+
+            return (
+                <Html>
+                <Head>
+                    <meta charSet="UTF-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                    {renderMeta.styleTags}
+                </Head>
+                <Body>{children}</Body>
+                </Html>
+            )
+        }
+    },
 }
