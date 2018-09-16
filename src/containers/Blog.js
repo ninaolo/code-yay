cff89cc17fbe03456b8056360985d4edf08a8a5f
Nina Olofsson <nina.olofsson@tink.se>
2018-09-16 21:55:02 +0200

    feat: pretty date for blog

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,19 +1,23 @@
-
 import React from 'react'
-import { withRouteData, Link } from 'react-static'
-//
+import {withRouteData, Link} from 'react-static'
 
-export default withRouteData(({ posts }) => (
-  <div>
-    <h1>It's blog time.</h1>
-    <br />
-    All Posts:
-    <ul>
-      {posts.map(post => (
-        <li key={post.slug}>
-            {post.date}: <Link to={`/blog/post/${post.slug}/`}>{post.title}</Link>
-        </li>
-      ))}
-    </ul>
-  </div>
+function prettyDate(date) {
+    date = new Date(date);
+    const month = date.getMonth() + 1;
+    return `${date.getFullYear()}-${month.toString().padStart(2, "0")}-${date.getDate()}`
+}
+
+export default withRouteData(({posts}) => (
+    <div>
+        <h1>It's blog time.</h1>
+        <br/>
+        All Posts:
+        <ul>
+            {posts.map(post => (
+                <li key={post.slug}>
+                    {prettyDate(post.date)}: <Link to={`/blog/post/${post.slug}/`}>{post.title}</Link>
+                </li>
+            ))}
+        </ul>
+    </div>
 ))
