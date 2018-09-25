074ca3c233bbe66d58b78b7c4f9ae32525df9dae
Nina Olofsson <nina.olofsson@tink.se>
2018-09-25 22:27:45 +0200

    chore: remove comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,8 @@
 module.exports = {
     extends: 'react-tools',
     rules: {
-        // Indent with 4 spaces
         "indent": ["error", 4],
-
-        // Indent JSX with 4 spaces
         "react/jsx-indent": ["error", 4],
-
-        // Indent props with 4 spaces
         "react/jsx-indent-props": ["error", 4],
     }
 }
