f14de9353a94bdf884d2a8628312d467e23c1e9b
Nina Olofsson <nina.olofsson@tink.se>
2018-09-25 22:28:42 +0200

    feat: add learn tree

--- static.config.js
+++ static.config.js
@@ -1,24 +1,88 @@
 import React from 'react'
-import {reloadRoutes} from 'react-static/node'
+import { reloadRoutes } from 'react-static/node'
 import jdown from 'jdown'
 import chokidar from 'chokidar'
-import {ServerStyleSheet} from 'styled-components'
+import { ServerStyleSheet } from 'styled-components'
 
 // Paths Aliases defined through tsconfig.json
 const typescriptWebpackPaths = require('./webpack.config.js')
 
 chokidar.watch('content').on('all', () => reloadRoutes())
 
+const learnTree = {
+    route: 'learn',
+    text: 'Learn',
+    fullPath: '/learn',
+    children: [
+        {
+            route: 'sorting',
+            text: 'Sorting',
+            children: [{
+                route: 'bubbelsort',
+                text: 'Bubble sort',
+                slug: 'bubble-sort'
+            }, {
+                route: 'heapsort',
+                text: 'Heap sort',
+                slug: 'heap-sort'
+            }, {
+                route: 'insertionsort',
+                text: 'Insertion sort',
+                slug: 'insertion-sort'
+            }, {
+                route: 'mergesort',
+                text: 'Merge sort',
+                slug: 'merge-sort'
+            }, {
+                route: 'quicksort',
+                text: 'Quick sort',
+                slug: 'quick-sort'
+            }]
+        },
+        {
+            route: 'problems',
+            text: 'Problems',
+            children: []
+        }, {
+            route: 'algorithms',
+            text: 'Algorithms',
+            children: []
+        }
+    ]
+}
+
+const getRoutesFromNode = (node, path, learnPostsBySlug) => {
+    node.fullPath = `${path}/${node.route}`
+    if (node.slug) {
+        node.content = learnPostsBySlug[node.slug]
+        console.log(learnPostsBySlug)
+    }
+
+    return {
+        path: `/${node.route}`,
+        component: node.slug ? 'src/components/TestComp' : 'src/containers/Learn',
+        getData: () => ({
+            node,
+        }),
+        children: node.children ? node.children.map(child => getRoutesFromNode(child, node.fullPath, learnPostsBySlug)) : []
+    }
+}
+
 export default {
-    siteRoot: "https://ninaolo.github.io",
-    basePath: "code-yay",
+    siteRoot: 'https://ninaolo.github.io',
+    basePath: 'code-yay',
 
     getSiteData: () => ({
-        title: 'React Static',
+        title: 'code-yay',
     }),
 
     getRoutes: async () => {
-        const {posts, home, about} = await jdown('content')
+        const { posts, home, about, learn } = await jdown('content')
+        const learnPostsBySlug = learn.reduce((map, post) => {
+            map[post.slug] = post
+            return map
+        }, {})
+
         return [
             {
                 path: '/',
@@ -37,6 +101,10 @@ export default {
             {
                 path: '/learn',
                 component: 'src/containers/Learn',
+                getData: () => ({
+                    node: learnTree,
+                }),
+                children: learnTree.children.map(node => (getRoutesFromNode(node, '/learn', learnPostsBySlug))),
             },
             {
                 path: '/blog',
@@ -51,11 +119,7 @@ export default {
                         post,
                     }),
                 })),
-            },
-            {
-                is404: true,
-                component: 'src/containers/404',
-            },
+            }
         ]
     },
 
@@ -67,7 +131,7 @@ export default {
     },
 
     Document: class CustomHtml extends React.Component {
-        render() {
+        render () {
             const {
                 Html, Head, Body, children, renderMeta,
             } = this.props
@@ -83,7 +147,8 @@ export default {
                 </Html>
             )
         }
-    }, webpack: (config, {defaultLoaders}) => {
+    },
+    webpack: (config, { defaultLoaders }) => {
         // Add .ts and .tsx extension to resolver
         config.resolve.extensions.push('.ts', '.tsx')
 
