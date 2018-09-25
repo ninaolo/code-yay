312ccbe9fca72efe8ec4619aebd21a32bdf06c67
Nina Olofsson <nina.olofsson@tink.se>
2018-09-25 22:10:23 +0200

    fix(button) Add innerRef

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import MuiButton, { ButtonProps } from '@material-ui/core/Button';
 import styled from 'styled-components';
 
 interface IProps extends ButtonProps {
+    innerRef?: (instance: any) => void,
     round?: boolean,
     box?: boolean
 }
 
-const StyledButton = styled(MuiButton) < IProps > `
+const StyledButton = styled(MuiButton) <IProps>`
   && {
     border-radius: ${props => (props.round ? '1.5rem' : '0')};
     padding: ${props => props.box ? '3rem' : '.75rem 3rem'};
@@ -15,10 +16,10 @@ const StyledButton = styled(MuiButton) < IProps > `
     text-transform: none;
     box-shadow: none;
   }
-`
+`;
 
 const Button: React.SFC<IProps> = props => (
     <StyledButton {...props} />
-)
+);
 
 export default Button;
