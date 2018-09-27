d15c73e03e7506a0963da00e02f13e03ca2b0c13
Nina Olofsson <nina.olofsson@tink.se>
2018-09-27 23:17:06 +0200

    fix: add types

--- src/containers/Learn.tsx
+++ src/containers/Learn.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import LinkButtonList from "@app/components/LinkButtonList";
 import { withRouteData } from "react-static";
+import { LearnTree } from "@app/content/learn";
 
-export default withRouteData(({node}) => {
-    const links = node.children.map(child => ({text: child.text, to: `${node.fullPath}/${child.route}`}));
+interface Data {
+    node: LearnTree
+}
+
+export default withRouteData((data: Data) => {
+    const tree = data.node;
+    const links = tree.children.map(child => ({text: child.text, to: `${tree.fullPath}/${child.route}`}));
     return (
         <div>
             <LinkButtonList links={links}/>
