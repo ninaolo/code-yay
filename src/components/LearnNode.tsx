0671876ea65471f8cc968f009ae7fabc760b1110
Nina Olofsson <nina.olofsson@tink.se>
2018-09-27 22:09:25 +0200

    feat: content in node

--- src/components/LearnNode.tsx
+++ src/components/LearnNode.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { withRouteData } from 'react-static'
 import convert from 'htmr'
 
-export default withRouteData(({ node }) => (
-    <div>
-        <h1>{node.title}</h1>
-            <p>{node.content ? convert(node.content.contents) : "Oops! Found no information here."}</p>
-    </div>
-))
+export default withRouteData(({node}) => {
+    return (
+        <div>
+            <h1>{node.content.title}</h1>
+
+            <h3>What?</h3>
+            <p>{node.content.what}</p>
+
+            <h3>How?</h3>
+            {node.content.how.split(" | ").map((h: string) => {
+                return <p>{h}</p>
+            })}
+
+            <h3>Implementation</h3>
+            <p>{convert(node.content.contents)}</p>
+
+        </div>
+    );
+})
