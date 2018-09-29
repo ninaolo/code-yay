f97ddcd9456b12b378e270a1ba8dc158bdc455a5
Nina Olofsson <nina.olofsson@tink.se>
2018-09-29 12:38:23 +0200

    feat(learn) more content

--- src/components/LearnNode.tsx
+++ src/components/LearnNode.tsx
@@ -15,6 +15,66 @@ export default withRouteData((learnNode: LearnNode) => {
         return "Oops! No information found here."
     }
 
+    const getHow = () => {
+        if (node.content && node.content.how) {
+            return (
+                <div>
+                    <h3>How?</h3>
+                    <ul>
+                        {node.content.how.split(" | ").map((h: string) => {
+                            return <li key={h}>{h}</li>
+                        })}
+                    </ul>
+                </div>
+            )
+        }
+        return ""
+    };
+
+    const getComplexity = () => {
+        if (node.content && node.content.bestTime) {
+            return (
+                <div>
+                    <h3>Complexity</h3>
+                    <ul>
+                        <li><b>Time best: </b>{node.content.bestTime}</li>
+                        <li><b>Time worst: </b>{node.content.worstTime}</li>
+                        <li><b>Time average: </b>{node.content.averageTime}</li>
+                        <li><b>Space: </b>{node.content.space}</li>
+                    </ul>
+                </div>
+            )
+        }
+        return ""
+    };
+
+    const getProsCons = () => {
+        if (node.content && node.content.pros && node.content.cons) {
+            return (
+                <div>
+                    <h3>Pros/cons</h3>
+                    <ul>
+                        <li>Pros:{node.content.pros}</li>
+                        <li>Cons:{node.content.cons}</li>
+                    </ul>
+                </div>
+            )
+        }
+        return ""
+    };
+
+    const getImplementation = () => {
+        if (node.content && node.content.contents) {
+            return (
+                <div>
+                    <h3>Implementation</h3>
+                    {convert(node.content.contents)}
+                </div>
+            )
+        }
+        return ""
+    };
+
     return (
         <div>
             <h1>{node.content.title}</h1>
@@ -22,16 +82,13 @@ export default withRouteData((learnNode: LearnNode) => {
             <h3>What?</h3>
             <p>{node.content.what}</p>
 
-            <h3>How?</h3>
-            {node.content.how.split(" | ").map((h: string) => {
-                return <p key={h}>{h}</p>
-            })}
+            {getHow()}
 
-            <h3>Complexity</h3>
-            {node.content.complexity}
+            {getComplexity()}
 
-            <h3>Implementation</h3>
-            {convert(node.content.contents)}
+            {getProsCons()}
+
+            {getImplementation()}
         </div>
     );
 })
