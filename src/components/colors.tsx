3b4a600a143a6c4251035c010b77d601af5d7d97
Nina Olofsson <nina.olofsson@tink.se>
2018-10-06 23:38:04 +0200

    feat(colors): add green + grey

--- src/components/colors.tsx
+++ src/components/colors.tsx
@@ -1,5 +1,41 @@
-const colors = {
-    lightGrey: "f4f4f4"
+const Green = {
+    50: "#39ae7f",
+    100: "#39ae7f",
+    200: "#39ae7f",
+    300: "#39ae7f",
+    400: "#39ae7f",
+    500: "#39ae7f",
+    600: "#39ae7f",
+    700: "#39ae7f",
+    800: "#39ae7f",
+    900: "#39ae7f",
+    A100: "#39ae7f",
+    A200: "#39ae7f",
+    A400: "#39ae7f",
+    A700: "#39ae7f",
 };
+
+const LightGrey = {
+    50: "#f4f4f4",
+    100: "#f4f4f4",
+    200: "#f4f4f4",
+    300: "#f4f4f4",
+    400: "#f4f4f4",
+    500: "#f4f4f4",
+    600: "#f4f4f4",
+    700: "#f4f4f4",
+    800: "#f4f4f4",
+    900: "#f4f4f4",
+    A100: "#f4f4f4",
+    A200: "#f4f4f4",
+    A400: "#f4f4f4",
+    A700: "#f4f4f4",
+};
+
+const colors = {
+    lightGrey: LightGrey,
+    green: Green,
+};
+
 
 export default colors
